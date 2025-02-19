f60e5cd30b8bf69c027a4b02065685ce66e2a185
Commondore <alex.som86@gmail.com>
2025-02-19 11:56:07 +0600

    add controls with logic

--- src/pages/pizza-page.tsx
+++ src/pages/pizza-page.tsx
@@ -2,6 +2,14 @@ import styles from "@/App.module.css";
 import { useState } from "react";
 import { Ings } from "@/interfaces/pizza";
 import { Pizza } from "@/components/pizza";
+import { Controls } from "@/components/controls";
+
+const PRICES: Record<string, number> = {
+  cheese: 30,
+  olives: 60,
+  sausage: 70,
+  mushrooms: 40,
+};
 
 export const PizzaPage = () => {
   const [ings, setIngs] = useState<Ings>({
@@ -11,10 +19,36 @@ export const PizzaPage = () => {
     mushrooms: 0,
   });
 
+  const [price, setPrice] = useState(100);
+
+  const addIng = (ingName: string) => {
+    setIngs((ings) => {
+      return {
+        ...ings,
+        [ingName]: ings[ingName] + 1,
+      };
+    });
+
+    setPrice((price) => price + PRICES[ingName]);
+  };
+
+  const removeIng = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, ingName: string) => {
+    event.stopPropagation();
+
+    setPrice((price) => price - PRICES[ingName] * ings[ingName]);
+
+    setIngs((ings) => {
+      return {
+        ...ings,
+        [ingName]: 0,
+      };
+    });
+  };
+
   return (
     <div className={styles.wrapper}>
       <Pizza ings={ings} />
-      <div>Кнопки добавления ингридиентов</div>
+      <Controls ings={ings} addIng={addIng} removeIng={removeIng} price={price} />
     </div>
   );
 };
